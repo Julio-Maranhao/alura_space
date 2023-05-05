2806c63ddb260e4ce493cd518a0ccfa1252d616b
Julio Maranhao <julio.maranhao@outlook.com>
2023-05-05 17:59:19 -0300

    commit aula models and admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")  # Fields to display
+    list_display_links = ("id","nome")  # Add link to edit item on that field 
+    search_fields = ('nome',)  # has to be a tuple for search
+    
+
+admin.site.register(Fotografia, ListandoFotografias)  # import Model and adminModel
+
