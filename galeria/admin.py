eb2a5496c8b424e1c4f6fc16dc6d9284a8b32e92
Julio Maranhao <julio.maranhao@outlook.com>
2023-05-09 11:21:53 -0300

    comit final curso 02

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,12 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")  # Fields to display
+    list_display = ("id", "nome", "legenda", "data_fotografia", "publicada")  # Fields to display
     list_display_links = ("id","nome")  # Add link to edit item on that field 
     search_fields = ('nome',)  # has to be a tuple for search
+    list_filter = ("categoria",) # has to be a tuple for filtering
+    list_editable = ("publicada",) #  Edit at table view
+    list_per_page = 10
     
 
 admin.site.register(Fotografia, ListandoFotografias)  # import Model and adminModel
